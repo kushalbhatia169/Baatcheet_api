0707e04d0513ba885e65d4a19f867bd2df4b8e65
kushalbhatia_jktech <kushalbhatia169@gmail.com>
2021-07-31 22:53:58 +0530

    eslint implemented

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express(),
-      //bodyParser = require("body-parser");
-      port = 3000,
-      cors = require('cors');
-      db = require('./db');
-      require('dotenv').config({path: __dirname + '/.env'});
+  //bodyParser = require("body-parser");
+  port = 3000,
+  cors = require('cors');
+db = require('./db');
+require('dotenv').config({path: __dirname + '/.env'});
 // place holder for the data
 const userRouter = require('./routes/router');
 
