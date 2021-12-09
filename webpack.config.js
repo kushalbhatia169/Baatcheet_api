c8a5d787ae4feccda9b111b5171fec6346df54b9
kushalbhatia_jktech <kushalbhatia169@gmail.com>
2021-12-09 09:36:39 +0530

    rf

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '.'),
     filename: 'server.bundle.js'
-  }
+  },
+  loader: "babel-loader", // or just "babel"
+  query: {
+    presets: [ <other presets />, 'stage-0']
+  },
 };
