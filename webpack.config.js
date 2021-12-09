e83373308a5fcce77b5faa661a7022c169ce3076
kushalbhatia_jktech <kushalbhatia169@gmail.com>
2021-12-09 11:16:06 +0530

    build added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 module.exports = {
   entry: './server.js',
   mode: 'production',
   target: 'node',
+  plugins: [
+    new CaseSensitivePathsPlugin()
+    // other plugins ...
+  ],
   output: {
     path: path.resolve(__dirname, '.'),
     filename: 'server.bundle.js'
