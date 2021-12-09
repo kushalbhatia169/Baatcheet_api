21ab03b31e212b603884d653087b2e3472bf6cfc
kushalbhatia_jktech <kushalbhatia169@gmail.com>
2021-12-09 09:25:23 +0530

    rf

--- controllers/GetChats.js
+++ controllers/GetChats.js
@@ -8,10 +8,10 @@ class GetChats {
                 { senderId: userId, recieverId: clientId },
                 { senderId: clientId, recieverId: userId }
             ]})
-            .sort({ createdAt: 1 })
+            .sort({ createdAt: 1 }) // sort by date
             .populate('chats') // multiple path names in one requires mongoose >= 3.6
             .then(async (usersDocuments) => {
-                return usersDocuments;
+                return usersDocuments; // return the array of users
             })
         .catch(err => {
             return new Error(err);
