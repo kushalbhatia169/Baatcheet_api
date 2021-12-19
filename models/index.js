48c668e97b8b09b8fd9b16441ba573170fe0708c
kushalbhatia_jktech <kushalbhatia169@gmail.com>
2021-12-19 17:18:36 +0530

    server build

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const dbconfig = require('../db/db.config');
+const uri = process.env.MONGODB_URI;
 mongoose
-  .connect(`${dbconfig.DBSERVICE}://${dbconfig.DBPORT}/${dbconfig.DBNAME}`, {...dbconfig.DBPARAMS})
+  .connect( uri || `${dbconfig.DBSERVICE}://${dbconfig.DBPORT}/${dbconfig.DBNAME}`, {...dbconfig.DBPARAMS})
   .then(() => console.log('connection successfull'))
   .catch(e => {
     console.error('Connection error', e.message)
