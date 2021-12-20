895ea8ba72bd71e68c56315291e3611e191caaea
kushalbhatia_jktech <kushalbhatia169@gmail.com>
2021-12-20 20:34:22 +0530

    server files changes

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser');
-const app = express(),
-  port = process.env.PORT || 8000,
-  cors = require('cors');
-  db = require('./models/index');
+const GetSingleUserByName = require('./controllers/GetSingleUserByName');
+const SaveMessage = require('./controllers/SaveMessage');
+const app = express();
+const port = process.env.PORT || 8000;
+const cors = require('cors');
+const db = require('./models/index');
 require('dotenv').config();   //to read the .env file
 // require('dotenv').config({path: __dirname + '/.env'});
 const userRouter = require('./routes/router');
@@ -22,6 +24,13 @@ app.use(express.static(path.join(__dirname, './server.bundle.js')));
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
 app.use('/api/user', userRouter)
 
 if ( process.env.NODE_ENV == "production"){ 
@@ -32,6 +41,51 @@ if ( process.env.NODE_ENV == "production"){
  }
 )};
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening on the port::${port}`);
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected to the WebSocket');
+  io.emit('message', 'Hello from the server');
+
+  // Emitting a new message. Will be consumed by the client
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+
+  socket.on('chat message', async (clientData) => {
+    console.log('Message from client: ', clientData);
+    const saveMsg = new SaveMessage();
+    try {
+      const messageData = {
+        message: clientData.msg,
+        senderId: clientData.senderId,
+        recieverId: clientData.recieverId,
+      }
+      await saveMsg.saveMessage(messageData)
+        .then((id)=>{
+          console.log(id)
+          io.emit('chat message', { msgId: id, message: clientData.msg, user: clientData.user,  
+            senderId: clientData.senderId, recieverId: clientData.recieverId });
+        })
+        .catch((err)=>{
+          throw new Error(err);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+  });
+
+  socket.on('getUser', async(clientData) => {
+    if(clientData?.user?.username){
+      const getUserInfo = new GetSingleUserByName();
+      try {
+        const user = await getUserInfo.getUserByName(clientData?.searchText);
+        user && io.emit('getUser', {user: user.username, _id: user._id});
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  })
 });
